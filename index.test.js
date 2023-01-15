7ecece051b64752b995afd89933dcd83769cce50
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-15 17:34:49 +0000

    Add require

--- index.test.js
+++ index.test.js
@@ -1,9 +1,11 @@
+const milesToKilometers = require('./')
+
 describe('milesToKilometers', function() {
   it('should convert miles to kilometers', function() {
-    expect(milesToKilometers(1)).to.equal(1.609);
-    expect(milesToKilometers(0)).to.equal(0);
+    expect(milesToKilometers(1)).to.be(1.609)
+    expect(milesToKilometers(0)).to.be(0)
   })
   it('should throw an error if miles is not a number', function() {
-    expect(milesToKilometers.bind(null, 'a')).to.throw('miles must be a number');
+    expect(milesToKilometers.bind(null, 'a')).to.throw('miles must be a number')
   })
 })
