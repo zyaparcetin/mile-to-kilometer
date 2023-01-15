290dc2520c9bdddea12b9ff560e751e7da3a2e23
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-15 17:46:33 +0000

    Delete null

--- index.test.js
+++ index.test.js
@@ -6,6 +6,6 @@ describe('milesToKilometers', function() {
     expect(milesToKilometers(0)).to.be(0)
   })
   it('should throw an error if miles is not a number', function() {
-    expect(milesToKilometers.bind(null, 'a')).to.throw('miles must be a number')
+    expect(milesToKilometers('a')).to.throw('miles must be a number')
   })
 })
