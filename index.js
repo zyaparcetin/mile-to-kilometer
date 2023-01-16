29d2d28cabd2d18d18acee64f24a7851dd5bd507
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-16 10:26:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-/* function milesToKilometers(miles) {
-  if (typeof miles !== 'number') {
-    throw new Error('miles must be a number');
-  } else {return miles * 1.609}
-  } */
-
-
-
-
-
-console.log(milesToKilometers(5))
-
 module.exports = milesToKilometers
+
+function milesToKilometers(miles) {
+  if (typeof miles !== 'number') {
+    throw new Error('miles must be a number!');
+  } return miles * 1.609
+} 
