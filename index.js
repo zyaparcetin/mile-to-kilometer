ebd567f28aa32e56919f550f18454bfeaab7c303
zyaparcetin <lavinia.zeynep35@gmail.com>
2023-01-16 10:17:26 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /* function milesToKilometers(miles) {
   if (typeof miles !== 'number') {
-    throw new Error('miles must be a number!');
+    throw new Error('miles must be a number');
   } else {return miles * 1.609}
   } */
 
-//const milesToKilometers = (miles) => typeof miles !== 'number' ? throw new Error('miles must be a number!') : miles * 1.609;
-//function milesToKilometers(miles) {typeof miles !== 'number' ? throw new Error('miles must be a number!') : return miles * 1.609}
 
-const milesToKilometers = (miles) => {if (typeof miles !== 'number') {throw new Error('miles must be a number!')} return miles * 1.609}
+
+
 
 console.log(milesToKilometers(5))
 
